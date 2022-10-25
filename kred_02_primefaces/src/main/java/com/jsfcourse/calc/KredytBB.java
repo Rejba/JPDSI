19daa2e214b85d90da1c13540a020179a4b657d6
Rejba <jppolona@o2.pl>
2022-10-25 21:08:01 +0200

    Add files via upload

---
+++ kred_02_primefaces/src/main/java/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,96 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String lat;
+	private String oprocentowanie;
+	private String wartosc;
+	private Double result;
+	private Double rata;
+
+	@Inject
+	FacesContext ctx;
+	
+	
+
+	public String getLat() {
+		return lat;
+	}
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+	public String getOprocentowanie() {
+		return oprocentowanie;
+	}
+
+	public void setOprocentowanie(String oprocentowanie) {
+		this.oprocentowanie = oprocentowanie;
+	}
+
+	public String getWartosc() {
+		return wartosc;
+	}
+
+	public void setWartosc(String wartosc) {
+		this.wartosc = wartosc;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double lat = Double.parseDouble(this.lat);
+			double wartosc = Double.parseDouble(this.wartosc);
+			double oprocentowanie = Double.parseDouble(this.oprocentowanie);
+			
+
+			result = (wartosc*(oprocentowanie/100)*lat)/(lat*12);
+			
+			
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Wynik: " + result, null));
+		}
+		return null;
+	}
+
+	public String info() {
+		return "info";
+	}
+}
